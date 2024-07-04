a3ccb81574421cdff8113318b0e136ba0a56b2cf
Bogdan Solga <bogdan.solga@gmail.com>
2024-07-04 11:52:30 +0300

    [dev] Added the @EnableMethodSecurity annotation

--- d05/d05s01-spring-security/src/main/java/net/safedata/springboot/training/d05/s01/config/SecurityConfiguration.java
+++ d05/d05s01-spring-security/src/main/java/net/safedata/springboot/training/d05/s01/config/SecurityConfiguration.java
@@ -6,6 +6,7 @@ import net.safedata.springboot.training.d05.s01.handler.SuccessfulAuthHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -17,6 +18,7 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfiguration {
 
     @Bean
