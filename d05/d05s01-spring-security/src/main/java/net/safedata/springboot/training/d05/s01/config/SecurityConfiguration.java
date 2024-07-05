9676c18fb44978c426e0797ac6f378d732142671
Bogdan Solga <bogdan.solga@gmail.com>
2024-07-05 10:20:02 +0300

    [improve] A few improvements

--- d05/d05s01-spring-security/src/main/java/net/safedata/springboot/training/d05/s01/config/SecurityConfiguration.java
+++ d05/d05s01-spring-security/src/main/java/net/safedata/springboot/training/d05/s01/config/SecurityConfiguration.java
@@ -9,6 +9,9 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
+import org.springframework.security.config.annotation.web.configurers.FormLoginConfigurer;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -23,44 +26,56 @@ public class SecurityConfiguration {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(authorizeRequests -> authorizeRequests.requestMatchers("/static/**").permitAll()
-                                                                         .requestMatchers(HttpMethod.POST, "/admin")
-                                                                            .hasAnyRole(Roles.ADMIN_ROLE, Roles.MANAGER_ROLE)
-                                                                         .requestMatchers(HttpMethod.GET, "/product")
-                                                                            .fullyAuthenticated()
-                                                                         .requestMatchers(HttpMethod.POST, "/product")
-                                                                            .hasAuthority("WRITE")
-                                                                         .anyRequest().authenticated())
-            .formLogin(formLogin -> formLogin.loginPage("/login")
-                                             .permitAll()
-                                             .successHandler(successfulAuthHandler())
-                                             .failureHandler(failedAuthHandler())
-                                             .defaultSuccessUrl("/")
-                                             .failureUrl("/login?error")
-                                             .usernameParameter("username")
-                                             .passwordParameter("password")
-                                             .permitAll())
-            .logout(logout -> logout.permitAll()
-                                    .deleteCookies("JSESSIONID")
-                                    .clearAuthentication(true)
-                                    .addLogoutHandler(postLogoutHandler())
-                                    .logoutSuccessUrl("/login?logout=true"));
+        http.authorizeHttpRequests(this::configureAuth)
+            .formLogin(this::configureFormLogin)
+            .logout(this::configureLogout);
 
         return http.build();
+    }
+
+    private AuthorizeHttpRequestsConfigurer<HttpSecurity>.AuthorizationManagerRequestMatcherRegistry
+            configureAuth(AuthorizeHttpRequestsConfigurer<HttpSecurity>.AuthorizationManagerRequestMatcherRegistry authorizeRequests) {
+        return authorizeRequests.requestMatchers("/static/**").permitAll()
+                                .requestMatchers(HttpMethod.POST, "/admin")
+                                .hasAnyRole(Roles.ADMIN_ROLE, Roles.MANAGER_ROLE)
+                                .requestMatchers(HttpMethod.GET, "/product")
+                                .fullyAuthenticated()
+                                .requestMatchers(HttpMethod.POST, "/product")
+                                .hasAuthority("WRITE")
+                                .anyRequest().authenticated();
+    }
+
+    private FormLoginConfigurer<HttpSecurity> configureFormLogin(FormLoginConfigurer<HttpSecurity> formLogin) {
+        return formLogin.loginPage("/login")
+                        .permitAll()
+                        .successHandler(successfulAuthHandler())
+                        .failureHandler(failedAuthHandler())
+                        .defaultSuccessUrl("/")
+                        .failureUrl("/login?error")
+                        .usernameParameter("username")
+                        .passwordParameter("password");
+    }
+
+    private LogoutConfigurer<HttpSecurity> configureLogout(LogoutConfigurer<HttpSecurity> logout) {
+        return logout.permitAll()
+                     .deleteCookies("JSESSIONID")
+                     .clearAuthentication(true)
+                     .addLogoutHandler(postLogoutHandler())
+                     .logoutSuccessUrl("/login?logout=true");
     }
 
     @Bean
     public UserDetailsService userDetailsService() {
         final InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-        manager.createUser(User.withUsername("user")
+        manager.createUser(User.withUsername("manager")
                                .password("password")
-                               .roles("USER")
-                               .authorities("WRITE")
+                               .roles(Roles.MANAGER_ROLE)
+                               .authorities("READ")
                                .passwordEncoder(pass -> passwordEncoder().encode(pass))
                                .build());
         manager.createUser(User.withUsername("admin")
                                .password("admin")
-                               .roles("ADMIN")
+                               .roles(Roles.ADMIN_ROLE)
                                .authorities("WRITE")
                                .passwordEncoder(pass -> passwordEncoder().encode(pass))
                                .build());
